f3cb504d6c1f40dd3220d0d158ecee9779a2f9a8
const_volatile <1043064987@qq.com>
2023-02-10 12:29:58 +0800

    🚧 item timeline

--- src/vue/dashboard/components/timeline.vue
+++ src/vue/dashboard/components/timeline.vue
@@ -1,5 +1,7 @@
 <script lang="ts">
 import type { AttachmentHistory } from 'zotero-reading-history';
+import { toTimeString, getTitle } from '../../utility/utils';
+
 export default {
     props: {
         history: {
@@ -7,13 +9,54 @@ export default {
             required: true,
         },
     },
+    watch: {
+        history(newHis: AttachmentHistory[]) {
+            this.items.length = 0;
+            const ha = new toolkit.HistoryAnalyzer(newHis),
+                firstTime = ha.firstTime,
+                lastTime = ha.lastTime;
+            if (firstTime && lastTime)
+                for (let i = firstTime; i <= lastTime; i += 86400) {
+                    const date = new Date(i * 1000),
+                        sec = ha.getByDate(date);
+                    if (sec > 0)
+                        this.items.push({
+                            date: date.toLocaleDateString(),
+                            time: toTimeString(sec),
+                            title: getTitle(newHis[0]), // TODO
+                        });
+                }
+        },
+    },
+    data() {
+        return {
+            items: new Array<{ date: string; time: string; title?: string }>(),
+        };
+    },
 };
 </script>
 
 <template>
-    <t-timeline>
-        <t-timeline-item></t-timeline-item>
-    </t-timeline>
+    <div class="timeline">
+        <t-timeline style="margin: auto">
+            <t-timeline-item
+                v-for="item of items"
+                :label="item.date"
+                dot-color="primary"
+            >
+                <t-tag theme="success" variant="light" max-width="260px">{{
+                    item.title
+                }}</t-tag>
+                <p>{{ item.time }}</p>
+            </t-timeline-item>
+        </t-timeline>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.timeline {
+    display: flex;
+    align-content: center;
+    padding: 10px;
+}
+</style>
--- src/vue/utility/utils.ts
+++ src/vue/utility/utils.ts
@@ -7,3 +7,22 @@ export function getTitle(his: AttachmentHistory) {
         ? ((item as Zotero.Item).getField('title') as string)
         : undefined;
 }
+
+function s2hour(s: number) {
+    return {
+        second: s % 60,
+        minute: Math.round(s / 60) % 60,
+        hour: Math.round(s / 3600),
+    };
+}
+
+export function toTimeString(seconds: number) {
+    const tim = s2hour(seconds);
+    let label = '';
+    if (tim.hour) label = tim.hour + toolkit.locale.hours;
+    if (tim.minute) label += tim.minute + toolkit.locale.minutes;
+    if (label.length < 1) label = seconds + toolkit.locale.seconds;
+    else if (!tim.hour && tim.second)
+        label += tim.second + toolkit.locale.seconds;
+    return label;
+}
