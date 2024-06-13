fc3bf56dfe25a398867f78516dac9a7e4a33eec2
volatile static <1043064987@qq.com>
2024-06-13 11:26:41 +0800

    ✨ auto update manifest release

--- tools/release.ts
+++ tools/release.ts
@@ -1,10 +1,13 @@
 import fs from 'fs';
 import path from 'path';
-//@ts-expect-error no types
 import NodeFormData from 'form-data';
 import { GiteeClient } from '@gitee/typescript-sdk-v5';
 import { Release } from 'zotero-plugin-scaffold';
 import loadConfig from './config';
+
+const owner = 'const_volatile',
+    repo = 'chartero',
+    client = new GiteeClient({ TOKEN: process.env.GITEE_TOKEN });
 
 main();
 
@@ -15,32 +18,52 @@ async function main() {
     if (!process.env.GITHUB_ACTIONS) return;
 
     const changelog = await releaser.getChangelog(),
-        client = new GiteeClient({ TOKEN: process.env.GITEE_TOKEN }),
-        release = await client.repositories.postV5ReposOwnerRepoReleases({
-            owner: 'const_volatile',
-            repo: 'chartero',
-            tagName: 'v' + releaser.version,
-            name: 'Release' + releaser.version,
-            targetCommitish: 'main',
-            body: changelog,
-        }),
-        form = new NodeFormData(),
-        xpi = fs.createReadStream(path.join(releaser.dist, `${releaser.xpiName}.xpi`));
-    form.append('file', xpi);
+        latestRelease = await rewriteRelease('v' + releaser.version, 'Release' + releaser.version, changelog),
+        updateRelease = await rewriteRelease(
+            'update',
+            'Zotero Auto Update Manifest',
+            `Updated in UTC ${new Date().toISOString()} for version ${releaser.version}.`,
+            true,
+        );
+    await uploadAsset(latestRelease.id!, path.join(releaser.dist, `${releaser.xpiName}.xpi`));
+    await uploadAsset(updateRelease.id!, path.join(releaser.dist, 'update.json'));
+}
 
-    const response = await client.repositories.httpRequest.request({
+async function rewriteRelease(tag: string, name: string, body: string, prerelease = false) {
+    const old = await client.repositories.getV5ReposOwnerRepoReleasesTagsTag({ owner, repo, tag });
+    if (old.id)
+        return client.repositories.patchV5ReposOwnerRepoReleasesId({
+            owner,
+            repo,
+            name,
+            body,
+            prerelease,
+            id: old.id,
+            tagName: tag,
+        });
+    return client.repositories.postV5ReposOwnerRepoReleases({
+        owner,
+        repo,
+        name,
+        body,
+        prerelease,
+        tagName: tag,
+        targetCommitish: 'main',
+    });
+}
+
+async function uploadAsset(release_id: number, file: string) {
+    const form = new NodeFormData(),
+        stream = fs.createReadStream(file);
+    form.append('file', stream);
+    return client.repositories.httpRequest.request({
         method: 'POST',
         url: '/v5/repos/{owner}/{repo}/releases/{release_id}/attach_files',
-        path: {
-            owner: 'const_volatile',
-            repo: 'chartero',
-            release_id: release.id,
-        },
+        path: { owner, repo, release_id },
         // formData: { file: xpi },
         // 因为gitee的api有问题，这里只能手动实现
         // gitee要求不是Blob就转string，但调用form-data时必须是stream。。
         headers: form.getHeaders(),
         body: form,
     });
-    releaser.logger.info(response);
 }
